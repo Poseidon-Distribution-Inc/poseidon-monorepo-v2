eb09a0f1ebfc57c4574f74b942676df5eae0b703
janjanXD <elnesjani@gmail.com>
2025-04-10 14:13:08 +0800

    new build

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,43 +1,42 @@
 type AuthProvider = "local" | "google";
 type UserRole = "admin" | "super_admin" | "subscriber" | "service_provider";
 interface IBaseUserSchema {
+    id:string
     email: string;
     phoneNumber?: string;
     role: UserRole;
     authProvider: AuthProvider;
-}
-
-export interface ILocalUserSchema extends IBaseUserSchema {
-    password: string;
-    isActive: boolean;
-}
-
-export interface ILocalUser extends ILocalUserSchema {
-    id: string;
     createdAt: Date;
     updatedAt: Date;
+    isActive: boolean;
 }
-
+export interface ILocalUserSchema extends IBaseUserSchema {
+    password: string;
+}
+export interface ILocalUser extends ILocalUserSchema {
+}
 export interface IGoogleUserSchema extends IBaseUserSchema {
     googleId: string;
     name: string;
     profilePicUrl: string;
-    isActive: boolean;
 }
-
 export interface IGoogleUser extends IGoogleUserSchema {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
 }
-
 export type IUserSchema = ILocalUserSchema | IGoogleUserSchema;
-
 export type IUser = ILocalUser | IGoogleUser;
-
-// API RES REQ TYPES
-
 export interface APIUserLoginRequest {
     message: string;
     data: IUser;
 }
+
+interface APIGetUserErrorResponse{
+    error:string;
+}
+
+interface APIGetUserSuccessResponse{
+    message:string;
+    data:IUser | IUser[]
+}
+
+export type APIUserResponse = APIGetUserSuccessResponse | APIGetUserErrorResponse
+export {};
