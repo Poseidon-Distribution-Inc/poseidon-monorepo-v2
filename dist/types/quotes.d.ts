b1e917d78015a88ca170400e2d2de1b282087c1f
jay descalsota <jaydescalsota@gmail.com>
2025-04-23 16:47:01 +0800

    add quote

---
+++ dist/types/quotes.d.ts
@@ -0,0 +1,28 @@
+export interface IQuoteSchema {
+    price: number;
+    currency: string;
+    quoterId: string;
+    postId: string;
+    isActive: boolean;
+    isAccepted: boolean;
+}
+export interface IQuote extends IQuoteSchema {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+interface APIQuoteErrorResponse {
+    error: string;
+}
+interface APIQuoteSuccessResponse {
+    message: string;
+    data: IQuote | IQuote[];
+}
+export type APIQuoteResponse = APIQuoteSuccessResponse | APIQuoteErrorResponse;
+export interface APICreateQuoteRequest {
+    quote: IQuoteSchema;
+}
+export interface APIUpdateQuoteRequest {
+    quote: Partial<IQuoteSchema>;
+}
+export {};
