e4b35b021cf58f8b7c6a51bfe989c5e4363ac8c8
jay descalsota <jaydescalsota@gmail.com>
2025-04-25 11:08:27 +0800

    build: update dist

---
+++ src/types/file.ts
@@ -0,0 +1,12 @@
+interface IFileSchema {
+  ownerId: string;
+  fileId: string;
+  publicLink: string;
+  type: "profile" | "company";
+}
+
+interface IFile extends IFileSchema {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
