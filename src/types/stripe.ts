6b01c414cedc5784ae637d18e2ede95f7e180862
jay descalsota <jaydescalsota@gmail.com>
2025-05-20 11:27:55 +0800

    build: update dist

---
+++ src/types/stripe.ts
@@ -0,0 +1,53 @@
+export interface IBankAccount {
+  type: string;
+  accountHolderName: string;
+  accountNumber: string;
+  routingNumber: string;
+  country: string;
+  currency: string;
+}
+
+export interface ICard {
+  type: string;
+  cardNumber: string;
+  expMonth: number;
+  expYear: number;
+  cvc: string;
+  country: string;
+  currency: string;
+}
+
+export interface IStripeBasic {
+  id: string;
+  userId: string;
+  stripeAccountId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export interface IStripeFullAccountSchema extends IStripeBasic {
+  accountType: "Personal" | "Business";
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  postalCode: string;
+  phoneNumber: string;
+
+  businessName: string;
+  businessType: "Individual" | "Company" | "Nonprofit" | "Government";
+  businessMCC: string;
+
+  businessTaxId: string;
+  businessTaxIdType: "EIN" | "SSN";
+
+  card: ICard | null;
+  bankAccount: IBankAccount | null;
+}
+
+export interface IStripeAccount extends IStripeFullAccountSchema {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
